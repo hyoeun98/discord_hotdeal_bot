4ea136032b40970791dd8451347e591b7c0c45a8
hyoeun98 <f2921641@naver.com>
2024-11-15 22:26:16 +0900

    python crawler, python scanner

--- crawler.py
+++ crawler.py
@@ -24,11 +24,34 @@ from dotenv import load_dotenv
 import os
 import base64
 import requests
-
+from scanner import PAGES, SITES
 load_dotenv()
 
+SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
+SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
+SLACK_CHANNEL_ID = os.environ.get("SLACK_CHANNEL_ID")
 DISCORD_WEBHOOK = os.environ.get("DISCORD_WEBHOOK")
+DISCORD_WEBHOOK = os.environ.get("DISCORD_WEBHOOK")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
 
+connection = psycopg2.connect(
+    dbname = DB_NAME,
+    user = DB_USER,
+    password = DB_PASSWORD,
+    host = DB_HOST,
+    port = DB_PORT
+)
+cursor = connection.cursor()
+client = WebClient(token=SLACK_TOKEN)
+
+crawling_error_insert_query = sql.SQL("""
+    INSERT INTO crawling_error (site_name_idx, error_log, timestamp, item_link)
+    VALUES (%s, %s, %s, %s)
+""")
 
 consumer = KafkaConsumer(
     'test', # 토픽명
@@ -55,24 +78,80 @@ class Crawler:
     def __init__(self):
         self.driver = set_driver()
             
+    def send_discord(self, **kwargs):
+        data = {
+            "content" :f"""page : {kwargs["page"]}
+            item_link : {kwargs["item_link"]}
+                        """
+        }
+        data = json.dumps(data)
+        response = requests.post(DISCORD_WEBHOOK, data = data, headers={"Content-Type": "application/json"})
+
+        # 응답 확인
+        if response.status_code == 204:
+            print("Message insert", kwargs["item_link"])
+        else:
+            print(f"Failed to send message: {response.status_code}, {response.text} {kwargs['item_link']}")
+            
+    def error_logging(self, e: Exception, error_type, item_link, **kwargs):
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')  # 현재 시간을 포맷팅
+        error_log = {"error_log": e, "time": timestamp, "error_type": error_type}
+        screenshot_filename = f'error_screenshot/{self.__class__.__name__}_{timestamp}.png'
+        PAGES.save_full_screenshot(self.driver, screenshot_filename)
+        
+        if kwargs:
+            for k, v in kwargs:
+                error_log[k] = v
+        
+        try:
+            with open(screenshot_filename, 'rb') as file:
+                response = client.files_upload_v2(
+                    channel=SLACK_CHANNEL_ID,
+                    file=file,
+                    filename=os.path.basename(screenshot_filename),  # 파일 이름
+                    initial_comment=error_log  # 업로드할 때 이미지 제목
+                )
+            print(f"File uploaded successfully: {response['file']['permalink']}")
+        except SlackApiError as e:
+            print(f"Error uploading file: {e.response['error']}")
+        
+        try:
+            cursor.execute(crawling_error_insert_query, (self.__class__.__name__, str(error_log), timestamp, item_link))
+            connection.commit()
+        except Exception as e:
+            print(e)
+            connection.rollback()
+    
     def consume_pages(self):
         for message in consumer:
             page = message.key
             item_link = message.value
-            timestamp = datetime.fromtimestamp(message.timestamp / 1000)
-            
-            data = {
-                "content" : message.value
-            }
-            data = json.dumps(data)
-            response = requests.post(DISCORD_WEBHOOK, data = data, headers={"Content-Type": "application/json"})
+            self.send_discord(page = page, item_link = item_link)
+            self.crawling(page, item_link)
 
-            # 응답 확인
-            if response.status_code == 204:
-                print("Message sent successfully!")
-            else:
-                print(f"Failed to send message: {response.status_code}, {response.text}")
+    def crawling(self, page, item_link):
+        if page not in SITES:
+            raise TypeError("Invalid page name")
+        
+        insert_table = page.lower()
+        crawling_result_insert_query = sql.SQL(f"""
+            INSERT INTO {insert_table} (created_at, item_name, item_link, shopping_mall, shopping_mall_link, price, delivery)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """)
+        
+        try:
+            result = SITES[page].crawling(self.driver, item_link)
+            value = (result["created_at"], result["item_name"], item_link, result["shopping_mall"], result["shopping_mall_link"], result["price"], result["delivery"])
+        except Exception as e:
+            self.error_logging(e, f"fail crawling {item_link}", item_link)
             
+        try:
+            cursor.execute(crawling_result_insert_query, value)
+            connection.commit()
+            print("success insert", item_link)
+        except Exception as e:
+            connection.rollback()
+            self.error_logging(e, f"fail insert{item_link}", item_link)
             
 crawler = Crawler()
 crawler.consume_pages()
