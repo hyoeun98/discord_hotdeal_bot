b59312365da1e5bd7332c1987a947b91f67afb92
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2025-05-07 16:31:39 +0900

    quasar zone category xpath 수정

--- lambda-selenium-docker-crawler/main.py
+++ lambda-selenium-docker-crawler/main.py
@@ -64,7 +64,7 @@ class QUASAR_ZONE(PAGES):
                 created_at = driver.find_element(By.CSS_SELECTOR, "#content > div > div.sub-content-wrap > div.left-con-wrap > div.common-view-wrap.market-info-view-wrap > div > dl > dt > div.util-area > p > span").text
                 content = driver.find_element(By.CSS_SELECTOR, "#new_contents").text
                 comment = list(map(lambda x: x.text, driver.find_elements(By.CSS_SELECTOR, "#content > div.sub-content-wrap > div.left-con-wrap > div.reply-wrap > div.reply-area > div.reply-list")))
-                category = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[3]/div/dl/dt/div[3]/div/div[1]").text
+                category = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[4]/div/dl/dt/div[3]/div/div[1]").text
                 shopping_mall_link = driver.find_elements(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[1]/td").text
                 shopping_mall = driver.find_elements(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[2]/td").text
                 price = driver.find_elements(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[3]/td").text
