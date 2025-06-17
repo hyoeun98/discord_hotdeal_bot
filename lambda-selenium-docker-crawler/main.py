1f742a0fa8613a698482f9927c5ec0664c291277
hyoeun98 <f2921641@naver.com>
2025-06-17 09:55:27 +0900

    edit: quasar zone shopping_mall, price, delivery xpath

--- lambda-selenium-docker-crawler/main.py
+++ lambda-selenium-docker-crawler/main.py
@@ -66,9 +66,9 @@ class QUASAR_ZONE(PAGES):
                 comment = list(map(lambda x: x.text, driver.find_elements(By.CSS_SELECTOR, "#content > div.sub-content-wrap > div.left-con-wrap > div.reply-wrap > div.reply-area > div.reply-list")))
                 category = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[1]/div[1]/div[4]/div/dl/dt/div[3]/div/div[1]").text
                 shopping_mall_link = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[1]/td/a").text
-                shopping_mall = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[2]/td").text
-                price = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[3]/td").text
-                delivery = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[4]/td").text
+                shopping_mall = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[2]/td").text
+                price = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[3]/td").text
+                delivery = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[1]/div[1]/div[4]/div/dl/dd/table/tbody/tr[4]/td").text
             except Exception as e:
                 print(f"fail get item link {item_link} {str(e)}")
                 
