0da1d67ab4608fbf4205ef61e258a7e7fa448b47
hyoeun98 <f2921641@naver.com>
2025-06-18 13:57:03 +0900

    cool enjoy shopping_mall 추출

--- lambda-selenium-docker-crawler/main.py
+++ lambda-selenium-docker-crawler/main.py
@@ -251,7 +251,9 @@ class COOL_ENJOY(PAGES):
                 comment = driver.find_element(By.ID, "bo_vc").text
                 created_at = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/article/section[1]/div[1]/ul/li[3]/time").text
                 shopping_mall_link = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/article/section[2]/ul/li/div/div/div[2]/a").get_attribute("href")
-
+                shopping_mall_candidate = re.search(r'\[([^]]+)\]', item_name)
+                if shopping_mall_candidate:
+                    shopping_mall = shopping_mall_candidate.group(1)
             except Exception as e:
                 print(f"fail get item link {item_link} {str(e)}")
                 
